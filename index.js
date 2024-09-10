7e37ad49d2e1d26458203dab6b270a137e8cc9d2
thomascansino <thomas.cansino@gmail.com>
2024-09-10 10:39:12 +0800

    feat: added necessary features to complete the project

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
-var app = express();
+const upload = multer();
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +14,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
+  const file = req.file;
+  const fileName = file.originalname;
+  const fileType = file.mimetype;
+  const fileSize = file.size;
 
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize,
+  });
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
